54af64b57ec1bf7003187b77181e62e893a946ab
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-01-29 09:00:10 -0300

    Sección 2: Web Personal - Proyecto Básico

--- WebPersonal/webpersonal/core/views.py
+++ WebPersonal/webpersonal/core/views.py
@@ -4,14 +4,12 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request,"core/home.html")
+    return render(request, "core/home.html")
 
 
 def about(request):
-    return render(request,"core/about.html")
+    return render(request, "core/about.html")
 
-def portafolio(request):
-    return render(request,"core/portafolio.html")
 
 def contacto(request):
-    return render(request,"core/contacto.html")
+    return render(request, "core/contacto.html")
