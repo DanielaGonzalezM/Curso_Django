28806b7a4f1a43dea9d4e3696b1fd6dcee89688f
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-02-25 18:27:22 -0300

    Sección 4: Web Playground - Proyecto Avanzado

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Mi Web Playground"
+        return context
+
+    def get(self, request, *arg, **kwargs):
+        return render(request, self.template_name, {"title": "Mi Web Playground 2"})
 
 
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
