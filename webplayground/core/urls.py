74a7e0676b4bdb4514560fa1abb7d45bb969c656
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-02-28 09:22:49 -0300

    Sección 4: Web Playground - Proyecto Avanzado

--- webplayground/core/urls.py
+++ webplayground/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import HomePageView, SamplePageView
+from pages.urls import pages_patterns
+
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("pages/", include("pages.urls"), name="home"),
+    path("pages/", include(pages_patterns)),
     path("sample/", SamplePageView.as_view(), name="sample"),
 ]
