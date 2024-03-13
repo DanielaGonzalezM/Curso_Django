8eb00c4ebb8012ffd4866a820e23ba5fafa00948
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-03-13 09:20:22 -0300

    Sección 4: Web Playground - Proyecto Avanzado

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Auth redirects
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "pages:pages"
+
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    # configurar un email real para producción
+    pass
