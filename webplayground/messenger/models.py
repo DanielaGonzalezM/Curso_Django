6da58ab286c36c7fb9251cba1e646b1142790e94
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-05-10 16:27:38 -0400

    Sección 4: Web Playground - Proyecto Avanzado

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Message(models.Model):
@@ -14,3 +15,19 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name="threads")
     messages = models.ManyToManyField(Message)
+
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    false_pk_set = set()
+    if action == "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                false_pk_set.add(msg_pk)
+    pk_set.difference_update(false_pk_set)
+
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
