ef1c542f1126d3c010b5894f3c09fc9fa2087997
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2024-05-13 16:09:55 -0400

    Sección 4: Web Playground - Proyecto Avanzado

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -30,19 +30,32 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name="threads")
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
+
     objects = ThreadManager()
+
+    class Meta:
+        ordering = ["-updated"]
 
 
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop("instance", None)
     action = kwargs.pop("action", None)
     pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
     false_pk_set = set()
-    if action == "pre_add":
+    if action is "pre_add":
         for msg_pk in pk_set:
             msg = Message.objects.get(pk=msg_pk)
             if msg.user not in instance.users.all():
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
                 false_pk_set.add(msg_pk)
+
+        # Forzar la actualización haciendo save
+        instance.save()
+
+    # Borramos de pk_set los mensajes que concuerdan con los de false_pk_set
     pk_set.difference_update(false_pk_set)
 
 
